773fb91700151131ffc5e0f72cd2aed206346830
AnujNegi157 <anujnegi157@gmail.com>
2024-01-12 23:22:28 +0530

    change

--- sc.js
+++ sc.js
@@ -6,7 +6,7 @@ const scrapeWebsite = async (baseUrl) => {
   const page = await browser.newPage();
 
   try {
-    // Navigate to the URL
+    
     await page.goto(baseUrl, { waitUntil: 'domcontentloaded' });
 
     const links = [];
@@ -16,27 +16,27 @@ const scrapeWebsite = async (baseUrl) => {
       const linkSelector = '.pointer.text-decoration-none.fas.fa-globe';
       const nextButtonSelector = '.pager-right-next.pager-item.pager-right';
 
-      // Wait for the link selector to appear on the page
+      
       const pageLinks = await page.$$eval(linkSelector, (links) => links.map((link) => link.href));
 
-      // Add the links to the array
+      
       links.push(...pageLinks);
     console.log(links);
-      // Save the links to the Excel sheet or perform any desired task
+     
       saveLinksToExcel(links);
 
-      // Wait for the next button selector to appear on the page
+      
       const [button] = await page.$$(nextButtonSelector);
 
-      // If the button is not found, break out of the loop
+     
       if (!button) {
         break;
       }
 
-      // Click on the second element
+      
       await button.click();
 
-      // You can add a delay if needed
+    
       await page.waitForTimeout(1000);
     }
 
@@ -48,10 +48,9 @@ const scrapeWebsite = async (baseUrl) => {
   }
 };
 
-// Function to save the links to Excel (you can customize this function)
+
 const saveLinksToExcel = (links) => {
-  // Implement your logic to save the links to Excel here
-  // Example using ExcelJS:
+ 
   const workbook = new ExcelJS.Workbook();
   const sheet = workbook.addWorksheet('Links');
   links.forEach((link) => {
@@ -60,6 +59,6 @@ const saveLinksToExcel = (links) => {
   workbook.xlsx.writeFile('links.xlsx');
 };
 
-// Example usage
+
 const baseUrl = 'https://cosmoprof2023.smallworldlabs.com/exhibitors#exhibitor-list';
 scrapeWebsite(baseUrl);
